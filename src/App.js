dcf4cc2c47fa0656632c44c2f7f26bc173f90837
SidR <68519263+SidCoderMan@users.noreply.github.com>
2025-06-23 19:02:03 -0500

    Learnt State in React with this project.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Get Started 🎬",
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+  "Total success 😎",
+];
+
+const App = () => {
+  const [currentStep, setCurrentStep] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const handlePrevious = () => {
+    if (currentStep > 0) setCurrentStep((step) => step - 1);
+  };
+
+  const handleNext = () => {
+    if (currentStep < 4) setCurrentStep((step) => step + 1);
+  };
+
+  const handleClose = () => {
+    setIsOpen((is) => !is);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={!isOpen ? "steps" : ""}>
+      <button className="close" onClick={handleClose}>
+        &times;
+      </button>
+      {isOpen ? (
+        <div className="steps">
+          <div className="numbers">
+            <div
+              className={
+                currentStep >= 1 && currentStep < 4
+                  ? "active"
+                  : currentStep === 4
+                  ? "success "
+                  : "numbers"
+              }
+            >
+              1
+            </div>
+            <div
+              className={
+                currentStep >= 2 && currentStep < 4
+                  ? "active"
+                  : currentStep === 4
+                  ? "success "
+                  : "numbers"
+              }
+            >
+              2
+            </div>
+            <div
+              className={
+                currentStep >= 3 && currentStep < 4
+                  ? "active"
+                  : currentStep === 4
+                  ? "success "
+                  : "numbers"
+              }
+            >
+              3
+            </div>
+          </div>
+
+          <p className="message">
+            Step {currentStep}: {messages[currentStep + 1 - 1]}
+          </p>
+
+          <div className="buttons">
+            <button
+              class={
+                currentStep === 4
+                  ? "success"
+                  : currentStep === 0
+                  ? "inactive"
+                  : ""
+              }
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              class={
+                currentStep === 4
+                  ? "success"
+                  : currentStep === 0
+                  ? "inactive"
+                  : ""
+              }
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      ) : (
+        <p className="message">{`Shhhh! This is a secret! 🤫`}</p>
+      )}
     </div>
   );
-}
-
+};
 export default App;
